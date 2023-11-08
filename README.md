d80202ad5f2eeb61b57bc6c8c530640ac5177f4e
Simon Liu <michaelblob99@gmail.com>
2023-11-07 23:30:23 -0800

    Initial code upload and update README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# dota-replays
+# DotA Replay Downloader
+
+A small script to query OpenDota for match information, request match replay parsing, and download replay files.
+
+## Dependencies
+
+ This script requires `wget` for file downloads, `pickle` for caching, and `tqdm` for progress visualization. These can be installed by running `pip3 install wget pickle tqdm`.
+
+ ## Usage
+
+Your `player_id` can be found by logging into [OpenDota](https://www.opendota.com/) and going to `My Profile`. The URL should be in the form of `https://www.opendota.com/players/[player_id]`. Update this value in the script to track player matches.
+
+Multiple players can be tracked simultaneously as the script uses the `player_id` in the cache filename. 
---
+++ dota_replays.py
@@ -0,0 +1,106 @@
+import os
+import sys
+import json
+import time
+import wget
+import pickle
+import requests
+from tqdm import tqdm
+from datetime import datetime
+
+class DotAReplays:
+    def __init__(self, player_id):
+        print(' . initiating DotAReplays instance for player %d' % player_id)
+        fn = './dr-%d.pkl' % player_id
+        if os.path.exists(fn):
+            print('   . found existing save at %s' % fn)
+            with open(fn, 'rb') as f:
+                saved = pickle.load(f)
+            # TODO: check if pkl contains necessary values
+            self.data = saved
+        else:
+            print('   . no save file found, initializing new instance')
+            self.data = {'player_id': player_id, 
+                         'matches': [], 
+                         'cache': {},
+                         'downloaded': []}
+
+    def get_matches(self):
+        print(' . getting matches')
+        time.sleep(0.8)
+        get = requests.get('https://api.opendota.com/api/players/%d/matches' % self.data['player_id'])
+        if get.status_code != 200:
+            sys.exit('  . error in fetching matches')
+        self.data['matches'] = json.loads(get.text)
+        print('   . found %d matches' % len(self.data['matches']))
+        
+
+    def get_details(self):
+        print(' . getting match details')
+        to_get = [match for match in self.data['matches'] if match['match_id'] not in self.data['cache']]
+        print('   . found %d matches not in cache' % len(to_get))
+
+        start = datetime.now()
+        failed = []
+        for match in tqdm(to_get):
+            time.sleep(0.8)
+            get = requests.get('https://api.opendota.com/api/matches/%d' % match['match_id'])
+            if get.status_code != 200:
+                failed.append(match['match_id'])
+                continue
+            self.data['cache'][match['match_id']] = json.loads(get.text)
+
+            # only request parse if match was within last 2 weeks
+            if (start - datetime.fromtimestamp(match['start_time'])).total_seconds() < 1209600:
+                time.sleep(0.8)
+                requests.post('https://api.opendota.com/api/request/%d' % match['match_id'])
+        
+        if failed:
+            print('   . could not get details for matches %s' % failed)
+
+    def get_downloads(self):
+        print(' . downloading match replays')
+        dir = './replays-%d/' % self.data['player_id']
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        # scan downloads
+        existing = [file for file in os.listdir(dir) if file[-8:] == '.dem.bz2' and int(file.split('/')[-1].split('_')[0]) in self.data['cache']]
+        print('   . found %d existing downloads' % len(existing))
+
+        start = datetime.now()
+        success = 0
+        failed = []
+        for match in tqdm(self.data['matches']):
+            if (start - datetime.fromtimestamp(match['start_time'])).total_seconds() < 1209600:
+                if match['match_id'] not in self.data['cache']:
+                    failed.append(match['match_id'])
+                    continue
+                replay_url = self.data['cache'][match['match_id']]['replay_url']
+                if replay_url.split('/')[-1] in existing: 
+                    continue
+                try:
+                    wget.download(replay_url, out=dir)
+                    success += 1
+                except:
+                    failed.append(match['match_id'])
+        if failed: print('   . downloaded %d matches, failed %d' % (success, len(failed)))
+    
+    def export(self):
+        print(' . exporting pickle')
+        fn = './dr-%d.pkl' % self.data['player_id']
+        with open(fn, 'wb') as f:
+            pickle.dump(self.data, f, protocol=pickle.HIGHEST_PROTOCOL)
+        print('   . saved to file %s' % fn)
+
+
+if __name__ == "__main__":
+    print('DotA Replay Downloader')
+    player_id = 0
+    if 'profile' not in json.loads(requests.get('https://api.opendota.com/api/players/%d' % player_id).text):
+        sys.exit(' . ERROR: player not found')
+
+    dr = DotAReplays(player_id)
+    dr.get_matches()
+    dr.get_details()
+    dr.get_downloads()
+    dr.export()
